a6c58a9a9c397ccc28d3039da4f6f3f4e4fa7210
Taipan <123_marcel@web.de>
2026-01-27 11:55:56 +0100

    FIX Ui is out of Vision cone & to big

--- UX/Assets/Scripts/JsonTextLoader.cs
+++ UX/Assets/Scripts/JsonTextLoader.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
-using static System.Net.Mime.MediaTypeNames;
 
 public class JsonTextLoader : MonoBehaviour
 {
@@ -17,6 +16,7 @@ public class JsonTextLoader : MonoBehaviour
     IEnumerator LoadText()
     {
         string path = System.IO.Path.Combine(UnityEngine.Application.streamingAssetsPath, fileName);
+        Debug.Log("PATH: " + path);
 
 #if UNITY_ANDROID && !UNITY_EDITOR
         UnityWebRequest request = UnityWebRequest.Get(path);
@@ -24,29 +24,41 @@ public class JsonTextLoader : MonoBehaviour
 
         if (request.result == UnityWebRequest.Result.Success)
         {
+            Debug.Log("JSON LOADED");
             ProcessJson(request.downloadHandler.text);
         }
         else
         {
+            Debug.Log("ERROR: " + request.error);
             textField.text = "File not found (Android).";
         }
 #else
         if (System.IO.File.Exists(path))
         {
             string json = System.IO.File.ReadAllText(path);
+            Debug.Log("JSON LOADED");
             ProcessJson(json);
         }
         else
         {
+            Debug.Log("FILE NOT FOUND");
             textField.text = "File not found.";
         }
 #endif
+
         yield break;
     }
 
     void ProcessJson(string json)
     {
         TextData data = JsonUtility.FromJson<TextData>(json);
+
+        if (data == null)
+        {
+            textField.text = "JSON Parse Error";
+            return;
+        }
+
         textField.text = $"<b>{data.title}</b>\n\n{data.message}";
     }
 }
