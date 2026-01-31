a9525c8cfb087843f5c27eb6909a4628f092c1b1
Taipan <123_marcel@web.de>
2026-01-31 13:19:21 +0100

    ADD WorkFlowData
    System.Serializable to phrase the json

---
+++ UX/Assets/Scripts/WorkflowData.cs
@@ -0,0 +1,14 @@
+[System.Serializable]
+public class WorkflowData
+{
+    public string Titel;
+    public string Point1;
+    public string Point2;
+    public string Point3;
+}
+
+[System.Serializable]
+public class VehicleList
+{
+    public VehicleData[] WorkflowItem;
+}
